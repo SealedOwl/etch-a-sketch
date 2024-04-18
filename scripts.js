588ded502fcd2d3414bb927b00cd74e86d911a89
SealedOwl <157732808+SealedOwl@users.noreply.github.com>
2024-04-18 17:49:40 +0530

    add proper sketch

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
+//Create the canvas to draw
 const container = document.querySelector('.container');
 const containerSize = 800;
 container.style.width=container.style.height=`${containerSize}px`
 
 const pixelSize = document.querySelector('#size');
 
+let isDrawing = false;
 
 function createGrid(size){
 
@@ -16,7 +18,14 @@ function createGrid(size){
             gridDiv.classList.add("gridStyle");
             gridDiv.style.width=gridDiv.style.height=`${(containerSize/size)}px`;
 
+            //Draw if mousedown and mouseover
+            gridDiv.addEventListener('mousedown',()=>{
+                isDrawing = true;
+            });
             gridDiv.addEventListener('mouseover',sketch);
+            gridDiv.addEventListener('mouseup',()=>{
+                isDrawing = false;
+            });
 
             container.appendChild(gridDiv);
         }
@@ -24,13 +33,18 @@ function createGrid(size){
 }
 
 
+
 function sketch(){
-    this.style.backgroundColor = 'black';
+    if(isDrawing){
+        this.style.backgroundColor = 'black';
+    }
 }
 
 
+//Create initial grid cells
 createGrid(parseInt(pixelSize.value));
 
+//Create cells according to given size
 pixelSize.addEventListener('input', ()=>{
     createGrid(parseInt(pixelSize.value));
 });
